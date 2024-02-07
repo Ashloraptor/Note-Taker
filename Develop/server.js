c745d81dbcce8732e7702a9439916b3bbf201196
Ash <ashloraptor@gmail.com>
2024-02-07 12:24:55 -0500

    Added index.js file.

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,31 @@
 //Import express package
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json');
+const api = require('./index.js');
 
 //Initialize app variable by setting it to value of express()
 const app = express();
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 // res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get('/api', (req, res) => res.json(termData));
+app.get('/api/notes', (req, res) => res.json(noteData));
 
 app.get('/', (req, res) => res.notes('Navigate to /notes'));
 
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET Route for main page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+//404 page catch
+app.get('*', (res, req) =>
+res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 );
 
 app.listen(PORT, () =>
