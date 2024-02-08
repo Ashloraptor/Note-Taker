bf345753c29b547e5626096b371f2b1a68f39458
Ash <ashloraptor@gmail.com>
2024-02-08 18:20:40 -0500

    Modified server page to better resemble example assignment 1

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,35 @@
 //Import express package
 const express = require('express');
+
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
+
+// Initialize an instance of Express.js
+const app = express();
+
+// Specify on which port the Express.js server will run
+const PORT = process.env.port || 3001;
+
+// Static middleware pointing to the public folder
+app.use(express.static('public'));
+
+// Create Express.js routes for default '/', '/send' and '/routes' endpoints
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.get('/send', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+);
+
+app.get('/routes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/routes.html'))
+);
+//End of example assignment 1
+
+//Does this connect to other necessary files within the program?
 const noteData = require('./db/db.json');
 const api = require('./index.js');
 
-//Initialize app variable by setting it to value of express()
-const app = express();
-const PORT = process.env.port || 3001;
+
 
 // res.json() allows us to return JSON instead of a buffer, string, or static file
 //app.get('/api/notes', (req, res) => res.json(noteData));
