212915e53518f88426976310a6d95beabb590cc6
Ash <ashloraptor@gmail.com>
2024-02-09 10:25:46 -0500

    Added utils file, apiRoutes file, htmlRoutes file

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 //Import express package
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
@@ -11,19 +13,23 @@ const app = express();
 const PORT = process.env.port || 3001;
 
 // Static middleware pointing to the public folder
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-//end of boilerplate code
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+
+
+
+
 
 // Create Express.js routes for default '/', '/send' and '/routes' endpoints
-app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
+//app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
 
-app.get('/index', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/assets/js/index.js')) //
-);
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
 //End of example assignment 1
 
 //Does this connect to other necessary files within the program?
@@ -33,22 +39,14 @@ app.get('/notes', (req, res) =>
 
 
 // // res.json() allows us to return JSON instead of a buffer, string, or static file
-// //app.get('/api/notes', (req, res) => res.json(noteData));
+//app.get('/api', (req, res) => res.json(noteData));
 // app.use(express.json());
 // //"it looks to me as though it's where the "routes" will be specified"
 // app.use('/api', api);
 
 // app.get('/', (req, res) => res.send('Navigate to /notes'));
 
-// // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
 
-// //GET Route for main page
-// app.get('/notes', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/notes.html'))
-// );
 
 //404 page catch
 app.get('*', (res, req) =>
