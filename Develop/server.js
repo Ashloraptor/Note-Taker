4631aa78d6d1b8c4c422bf4e2910ab2b1d5ac2bd
Ash <ashloraptor@gmail.com>
2024-02-09 09:32:50 -0500

    Got the files to route from index to notes at the click of a button.

--- Develop/server.js
+++ Develop/server.js
@@ -12,41 +12,43 @@ const PORT = process.env.port || 3001;
 
 // Static middleware pointing to the public folder
 app.use(express.static('public'));
+//end of boilerplate code
 
 // Create Express.js routes for default '/', '/send' and '/routes' endpoints
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
 
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/assets/js/index.js')) //
 );
 
-app.get('/routes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/routes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 //End of example assignment 1
 
 //Does this connect to other necessary files within the program?
-const noteData = require('./db/db.json');
-const api = require('./index.js');
+// const noteData = require('./db/db.json');
+// const api = require('public/assets/js/index.js');
 
 
 
-// res.json() allows us to return JSON instead of a buffer, string, or static file
-//app.get('/api/notes', (req, res) => res.json(noteData));
-app.use(express.json());
-app.use('/api', api);
+// // res.json() allows us to return JSON instead of a buffer, string, or static file
+// //app.get('/api/notes', (req, res) => res.json(noteData));
+// app.use(express.json());
+// //"it looks to me as though it's where the "routes" will be specified"
+// app.use('/api', api);
 
-app.get('/', (req, res) => res.send('Navigate to /notes'));
+// app.get('/', (req, res) => res.send('Navigate to /notes'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// // GET Route for homepage
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
-//GET Route for main page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+// //GET Route for main page
+// app.get('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/notes.html'))
+// );
 
 //404 page catch
 app.get('*', (res, req) =>
